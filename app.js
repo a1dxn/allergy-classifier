1a791409d0c1079f9418a7769b9e1af052a8e683
Aidan <1712906@brunel.ac.uk>
2020-12-30 22:57:42 +0000

    allergy-classifier-29 Add authorisation to api endpoint

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const createError  = require("http-errors");
-const express      = require("express");
-const path         = require("path");
-const cookieParser = require("cookie-parser");
-const logger       = require("morgan");
+const createError    = require("http-errors");
+const express        = require("express");
+const path           = require("path");
+const logger         = require("morgan");
+const passport       = require("passport");
+const BearerStrategy = require("passport-http-bearer");
 
 const app = express();
 
@@ -13,8 +14,17 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+//Authorisation with passport bearer
+passport.use(new BearerStrategy(
+	(token, done) => {
+		const control = process.env.API_PRIVILEGED_SECRET;
+		if(control && token!==control) return done(null, false);
+		return done(null, true);
+	}
+));
+app.use(passport.initialize());
 
 //Index routes
 app.use("/", require("./routes/index"));
@@ -26,18 +36,18 @@ if(!process.env.API_LOCAL) {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error   = req.app.get("env")==="development" ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error   = req.app.get("env")==="development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
